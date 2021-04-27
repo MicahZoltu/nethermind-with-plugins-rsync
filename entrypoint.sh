ae89b0c13b9480018fc910b7d051f95c3cae25a4
Scott Bigelow <epheph@gmail.com>
2021-04-27 12:38:46 -0600

    rsync needs to delete or you'll end up with additive sync's. Standardize SSH_CMD

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
-#!/bin/bash -x
+#!/bin/sh
 set -e
 
 NETHERMIND_DATA_DIR=/nethermind/nethermind_db/mainnet
 LAUNCH_NETHERMIND="exec ./Nethermind.Runner"
+SSH_CMD="ssh -i $NETHERMIND_SYNC_SSH_KEY_PATH -o StrictHostKeyChecking=no -p $NETHERMIND_SYNC_PORT"
 
 if [ -d "$NETHERMIND_DATA_DIR" ] && [ "$NETHERMIND_SYNC_FORCE" != "true" ]; then
 	echo "Data directory already exists and NETHERMIND_SYNC_FORCE != true"
@@ -10,14 +11,14 @@ if [ -d "$NETHERMIND_DATA_DIR" ] && [ "$NETHERMIND_SYNC_FORCE" != "true" ]; then
 fi
 
 if [ "$NETHERMIND_SYNC_SNAP_OVERRIDE" = "" ]; then
-	SNAPSHOT=`ssh -i $NETHERMIND_SYNC_SSH_KEY_PATH -o StrictHostKeyChecking=no nethermind@$NETHERMIND_SYNC_SERVER -p $NETHERMIND_SYNC_PORT ls -1 /nethermind | tail -n 1`
+	SNAPSHOT=`$SSH_CMD nethermind@$NETHERMIND_SYNC_SERVER ls -1 /nethermind | tail -n 1`
 	echo "Fetching newest snapshot: $SNAPSHOT"
 else
 	SNAPSHOT=$NETHERMIND_SYNC_SNAP_OVERRIDE
 	echo "Fetching override snapshot: $SNAPSHOT"
 fi
 
-rsync -av -e "ssh -i $NETHERMIND_SYNC_SSH_KEY_PATH -o StrictHostKeyChecking=no -p $NETHERMIND_SYNC_PORT" nethermind@$NETHERMIND_SYNC_SERVER:/nethermind/$SNAPSHOT/mainnet/ $NETHERMIND_DATA_DIR
+rsync -av --delete -e "$SSH_CMD" nethermind@$NETHERMIND_SYNC_SERVER:/nethermind/$SNAPSHOT/mainnet/ $NETHERMIND_DATA_DIR
 
 echo "rsync complete, launching Nethermind"
 $LAUNCH_NETHERMIND
